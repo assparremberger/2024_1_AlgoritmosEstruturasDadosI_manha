73e9b316e9d771eed3d5562a1f049b6b2a13c9b5
ADALTO SELAU SPARREMBERGER <assparremberger@fspoa.br>
2024-04-10 11:09:03 -0300

    Aula06 - Encapsulamento

---
+++ Aula06-Encapsulamento/main.py
@@ -0,0 +1,15 @@
+from Conta import Conta
+c = Conta()
+# print( "Saldo: " , c.getSaldo() )
+# c.setSaldo( 100 )
+# print( "Saldo: " , c.getSaldo() )
+
+print( "Saldo: " , c.saldo )
+c.saldo = 100
+print( "Saldo: " , c.saldo ) 
+
+c.sacar( 100 )
+c.sacar( 98 )
+c.depositar( 1.5 )
+c.depositar( 2 )
+print( f'Saldo: {c.saldo:.2f}' ) 
